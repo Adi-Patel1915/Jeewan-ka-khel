cde433c9089492078b984bd6ea3c98b8a61a796d
Aditya Patel <110101372+Adi-Patel1915@users.noreply.github.com>
2024-03-16 17:38:32 -0600

    test file ad logic created program is slow but works

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 import pygame as game
+import numpy
 import random as rndm
 #--------------------
 game.init()
-Width = 500
-Height = 500
+Width = 1920
+Height = 1200
 
-CellSize = 10
+CellSize = 20
 StateColor = ((0, 0, 0), (255, 255, 255))
 Canvas = game.display.set_mode((Width, Height))
-game.display.set_caption("Canvas")
+game.display.set_caption("Game of Life X Game Theory")
 Switch = True
 Clock = game.time.Clock()
-FPS = 144 #FPS here
+FPS = 60 #FPS here
 #--------------------
 class CELL():
     def __init__(self, x, y): # will add a future parameter state
@@ -29,9 +30,38 @@ class CELL():
 
 #--------------------
 celllist = []
-for i in range(0, Width - 9, CellSize):
-    for j in range(0, Height - 9, CellSize):
-        celllist.append(CELL(i, j))
+for i in range(0, (Width - CellSize) + 1, CellSize):
+    temp = []
+    for j in range(0, (Height - CellSize) + 1, CellSize):
+        temp.append(CELL(i, j))
+    
+    celllist.append(temp)
+#-------------------------------------------
+def NextState(Celllist):
+    CurrentState = Celllist.copy()
+    for i in range(len(CurrentState)):
+        for j in range(len(CurrentState[0])):
+            neighbor = 0
+            for xoffset in range(-1,2):
+                for yoffset in range(-1,2):
+                    if (i + xoffset < 0) or (i + xoffset > len(CurrentState) - 1) or (j + yoffset < 0) or (j + yoffset > len(CurrentState[0]) - 1) or (xoffset == 0 and yoffset == 0):
+                        continue
+                    elif CurrentState[i + xoffset][j + yoffset].state == 1:
+                        neighbor += 1
+            
+            if neighbor <= 1 and Celllist[i][j].state == 1:
+                Celllist[i][j].state = 0
+            elif neighbor >= 4 and Celllist[i][j].state == 1:
+                Celllist[i][j].state = 0
+            elif (neighbor == 2 or neighbor == 3) and Celllist[i][j].state == 1:
+                Celllist[i][j].state = 1
+            elif neighbor == 3 and Celllist[i][j].state == 0:
+                Celllist[i][j].state = 1
+
+
+    return None
+
+#-------------------------------------------
 while Switch:
     events = game.event.get()
     for event in events:
@@ -39,8 +69,9 @@ while Switch:
             Switch = False
 
     for i in celllist:
-        i.Draw()
+        for j in i:
+            j.Draw()
 
-    
+    NextState(celllist)
     game.display.update()
     Clock.tick(FPS)
